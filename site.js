ed9cf31e79db8afe1dda9c09164a7a5d22055468
Jude Amboise <amboiseyide@gmail.com>
2024-04-10 16:42:25 -0500

    Added javascript code

--- site.js
+++ site.js
@@ -1,4 +1,17 @@
-//This is not showing up  
-const showNextJoke = () => {
 
+const showNextJoke = () => {
+    fetch('https://api.chucknorris.io/jokes/random?category=movie') // This line of code is used to fetch the data from the API
+        .then(response => response.json())
+        .then(data => {
+            const jokeDiv = document.getElementById('joke');
+            jokeDiv.innerText = data.value;
+        })
+        .catch(error => {
+            console.log('Error:', error);
+        });
 }
+showNextJoke();
+const button = document.querySelector('button');
+button.addEventListener('click', showNextJoke);
+//cosole log the result her as well
+console.log(showNextJoke);
